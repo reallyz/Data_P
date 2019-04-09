305c8b4159e3a2b8779bdc79e0a2eda29594403a
Ruili <rightwrite@163.com>
2019-04-09 16:16:39 +0800

    more details about PCA

--- scratch/algriothmscratch.py
+++ scratch/algriothmscratch.py
@@ -1,9 +1,57 @@
 import numpy as np
 from sklearn.decomposition import  PCA
 import pandas as pd
+import  random as rd
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
 #TODO
 #降维
+#introduce
+#create data
+plt.rcParams['axes.unicode_minus']=False
+genes=['gene'+str(i) for i in range(1,101)]
+wt=['wt'+str(i) for i in range(1,6)]
+ko=['ko'+str(i) for i in range(1,6)]
+datamk=pd.DataFrame(columns=[*wt,*ko],index=genes)
+for gene in datamk.index:
+    datamk.loc[gene,'wt1':'wt5']=np.random.poisson(lam=rd.randrange(10,1000),size=5)
+    datamk.loc[gene,'ko1':'ko5']=np.random.poisson(lam=rd.randrange(10,1000),size=5)
+#center and scale data,after centering the average value for each gene will be 0
+#after sacling,the standard deviation for the values for each gene will be 1
+scaled_data=preprocessing.scale(datamk.T) #or StandardScaler().fit_transform(datamk,T)
+pca=PCA()#create PCA object
+pca.fit(scaled_data)#do the PCA math
+pca_data=pca.fit_transform(scaled_data)#generate coordinates for PCA graph
+#scree plot
+per_var=np.round(pca.explained_variance_ratio_*100,decimals=1) #calculate the percentage of variation that each principal
+#component accounts for
+labels=['PC'+str(i) for i in range(1,len(per_var)+1)]
+plt.bar(x=range(1,len(per_var)+1),height=per_var,tick_label=labels)
+plt.xlabel('Principal Component')
+plt.ylabel('Percentage of Explained Variance')
+plt.title('Scree Plot')
+
+#starat to draw PCA plot
+plt.figure('PCA Graph')
+pca_df = pd.DataFrame(pca_data,index=[*wt,*ko],columns=labels)
+plt.scatter(pca_df.PC1,pca_df.PC2)
+plt.title('PCA Graph')
+plt.xlabel('PC1-{}%'.format(per_var[0]))
+plt.ylabel('PC2-{}%'.format(per_var[1]))
+for sample in pca_df.index:
+    plt.annotate(sample,(pca_df.PC1.loc[sample],pca_df.PC2.loc[sample]))
+#show the loading scores for PC1 to determine which genes
+#had the largest influence on separating the two clusters along the x-axis
+loading_scores=pd.Series(pca.components_[0],index=genes)
+sorted_loading_scores=loading_scores.abs().sort_values(ascending=False)
+top_10_genes=sorted_loading_scores[0:10].index.values
+print(loading_scores[top_10_genes])
+
+plt.show()
+
+
+
 #PCA
 data = np.array([np.array([2.5,0.5,2.2,1.9,3.1,2.3,2,1,1.5,1.1]),
                 np.array([2.4,0.7,2.9,2.2,3,2.7,1.6,1.1,1.6,0.9])]).T
@@ -13,6 +61,7 @@ lower_dim =PCA(n_components=1)
 lower_dim.fit(data)
 print('information ratio:\n',lower_dim.explained_variance_ratio_)
 print('transformed data:\n',lower_dim.transform(data))
+print('fit_transform:\n',lower_dim.fit_transform(data),'\n\n')
 #主成分分析
 def myPCA(data,n_componets=10000):
     mean_vals=np.mean(data,axis=0)
