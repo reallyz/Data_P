1c5fefa4818200dde35a70097021f466097b02b4
HotPot <zhuruilicust@163.com>
2019-05-05 16:34:15 +0800

    聚类分析

--- scratch/cluster.py
+++ scratch/cluster.py
@@ -1,17 +1,17 @@
 import numpy as np
 import  matplotlib.pyplot as plt
 from sklearn.datasets import make_circles,make_blobs,make_moons
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans,DBSCAN,AgglomerativeClustering
 
 n_s=1000
 circles=make_circles(n_samples=n_s,factor=0.5,noise=0.05)
 moons=make_moons(n_samples=n_s,noise=0.05)
-blobs=make_blobs(n_samples=n_s,random_state=8)
+blobs=make_blobs(n_samples=n_s,random_state=8,center_box=(-1,1),cluster_std=0.1)
 random_data=np.random.rand(n_s,2),None
 #t=None,None,type(t)==tuple
 colors='bgrcmyk'
 data=[circles,moons,blobs,random_data]
-models=[('None',None),('Kmeans',KMeans(n_clusters=2))]
+models=[('None',None),('Kmeans',KMeans(n_clusters=2)),('DBSCAN',DBSCAN(min_samples=3,eps=0.2)),('AGG',AgglomerativeClustering(n_clusters=3))]
 f=plt.figure()
 plt.rcParams['axes.unicode_minus']=False
 for index,clt in enumerate(models):
@@ -22,7 +22,7 @@ for index,clt in enumerate(models):
             clt_res=[0 for i in range(len(X))]
         else:
             clt_entity.fit(X)
-            clt_res=clt_entity.labels_.astype(np.int)
+            clt_res=clt_entity.labels_
         plt.title(clt_name)
         f.add_subplot(len(models),len(data),index*len(data)+indexs+1)
         plt.title(clt_name)
