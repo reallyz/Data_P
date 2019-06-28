c71f48252f64098f1371529d1df2f95d1d0bfd4a
HotPot <zhuruilicust@163.com>
2019-06-28 12:10:46 +0800

    dataFrame.apply()

--- 新人赛o2o/roughcode.py
+++ 新人赛o2o/roughcode.py
@@ -2,5 +2,13 @@
 
 import pandas as pd
 
-df=pd.read_csv('./ccf_offline_stage1_train/ccf_offline_stage1_train.csv',date_parser=['Date_received','Date'])
-coup=df['Coupon_id']
+df=pd.read_csv('./ccf_offline_stage1_train/ccf_offline_stage1_train.csv')
+
+def label(df):
+    if pd.to_datetime(df['Date'],format='%Y%m%d')-pd.to_datetime(df['Date_received'],format='%Y%m%d') <= pd.Timedelta(15,'D'):
+        return 1
+    else:
+        return 0
+df['label']=df.apply(label,axis=1)
+print(df['label'].head(20))
+print(df['label'].value_counts())
